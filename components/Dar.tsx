219db22138ced516aae9e7993bb8228c984fad77
sorrycc <sorrycc@gmail.com>
2021-07-08 18:16:19 +0800

    chore: update

---
+++ components/Dar.tsx
@@ -0,0 +1,10 @@
+import { memo } from 'react';
+
+export default memo(
+  (props) => {
+    return <div>Dar</div>
+  },
+  () => {
+    return true;
+  },
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './index.less';
 import Foo from '../components/Foo';
+import Dar from "../components/Dar";
 
 export default function Page() {
   return (
     <div>
-      <h1 className={styles.title}>Page index 2</h1>
+      <h1 className={styles.title}>Page index 2333443</h1>
       <Foo />
+      <Dar />
     </div>
   );
 }
